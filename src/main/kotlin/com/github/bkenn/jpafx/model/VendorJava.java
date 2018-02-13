077a3cb68285efc6d31a457b6f6989679e6ca957
briank <briankennedy392@gmail.com>
2018-02-13 16:17:49 -0500

    testing java version

---
+++ src/main/kotlin/com/github/bkenn/jpafx/model/VendorJava.java
@@ -0,0 +1,52 @@
+package com.github.bkenn.jpafx.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import javax.persistence.*;
+
+@Entity
+public class VendorJava {
+
+    @Transient
+    private final IntegerProperty idProperty = new SimpleIntegerProperty();
+
+    @Transient
+    private final StringProperty nameProperty = new SimpleStringProperty();
+
+    public VendorJava() { }
+
+    public VendorJava(String name) {
+        setName(name);
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return idProperty.get();
+    }
+
+    public void setId(Integer id) {
+        idProperty.set(id);
+    }
+
+    public IntegerProperty idProperty() {
+        return idProperty;
+    }
+
+    @Column
+    public String getName() {
+        return this.nameProperty.getValue();
+    }
+
+    public void setName(String name) {
+        this.nameProperty.setValue(name);
+    }
+
+    public StringProperty nameProperty() {
+        return nameProperty;
+    }
+
+}
---
+++ src/main/kotlin/com/github/bkenn/jpafx/view/JavaView.kt
@@ -0,0 +1,71 @@
+package com.github.bkenn.jpafx.view
+
+import com.github.bkenn.jpafx.Styles
+import com.github.bkenn.jpafx.data.VendorJavaRepository
+import com.github.bkenn.jpafx.data.VendorRepository
+import com.github.bkenn.jpafx.model.Vendor
+import com.github.bkenn.jpafx.model.VendorJava
+import javafx.collections.FXCollections
+import javafx.geometry.Pos
+import tornadofx.*
+
+class JavaView : View("Hello TornadoFX") {
+
+    val vendorRepository: VendorJavaRepository by di()
+    val vendors = FXCollections.observableArrayList<VendorJava>()
+
+    init {
+        vendorRepository.save(VendorJava("Oracle"))
+        vendorRepository.save(VendorJava("Google"))
+        vendors.addAll(vendorRepository.findAll())
+    }
+
+    override val root = vbox {
+        style {
+            alignment = Pos.CENTER
+        }
+        label(title) {
+            addClass(Styles.heading)
+        }
+        tableview(vendors) {
+            column("ID", VendorJava::idProperty)
+            column("Name", VendorJava::nameProperty)
+        }
+    }
+
+}
+
+/*
+import com.github.bkenn.jpafx.Styles
+import com.github.bkenn.jpafx.data.VendorRepository
+import com.github.bkenn.jpafx.model.Vendor
+import com.github.bkenn.jpafx.model.VendorJava
+import javafx.collections.FXCollections
+import javafx.geometry.Pos
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+
+    val vendorRepository: VendorRepository by di()
+    val vendors = FXCollections.observableArrayList<VendorJava>()
+
+    init {
+        vendorRepository.save(VendorJava())
+        vendorRepository.save(VendorJava())
+        vendors.addAll(vendorRepository.findAll())
+    }
+
+    override val root = vbox {
+        style {
+            alignment = Pos.CENTER
+        }
+        label(title) {
+            addClass(Styles.heading)
+        }
+        tableview(vendors) {
+            column("ID", VendorJava::idProperty)
+        }
+    }
+
+}
+ */
