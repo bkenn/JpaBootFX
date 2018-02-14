6ecaae5c10575d97a3a811cbf79b4b69d6b24f60
brian <briankennedy392@gmail.com>
2018-02-14 17:25:38 -0500

    needed to fix save of entities in MainView to showcase save

--- src/main/kotlin/com/github/bkenn/jpafx/view/MainView.kt
+++ src/main/kotlin/com/github/bkenn/jpafx/view/MainView.kt
@@ -1,6 +1,7 @@
 package com.github.bkenn.jpafx.view
 
 import com.github.bkenn.jpafx.Styles
+import com.github.bkenn.jpafx.data.CustomerRepository
 import com.github.bkenn.jpafx.data.VendorRepository
 import com.github.bkenn.jpafx.model.Customer
 import com.github.bkenn.jpafx.model.Vendor
@@ -10,13 +11,14 @@ import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
 
+    val customerRepository: CustomerRepository by di()
     val vendorRepository: VendorRepository by di()
     val vendors = FXCollections.observableArrayList<Vendor>()
 
     init {
-        val customers = listOf(Customer("Janet"), Customer("Steve"))
+        val psCustomers = customerRepository.saveAll(listOf(Customer("Janet"), Customer("Steve")))
         val jetbrains = Vendor("Jetbrains")
-        jetbrains.customers.addAll(customers)
+        jetbrains.customers.addAll(psCustomers)
         vendorRepository.save(jetbrains)
         vendors.addAll(vendorRepository.findAll())
     }
